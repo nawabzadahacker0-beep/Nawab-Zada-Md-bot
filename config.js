c3ce92c56ad4f2aee4067c4889d05ff75ce791b4
nawabzadahacker0-beep <nawabzadahacker0@gmail.com>
2026-06-21 00:01:51 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+const config = {
+  botName: "᭄ꪶꪀꪖ᭙ꪖ᥇ ⤹ꪑᦔ ᥇ꪮꪻ ✿",
+  ownerName: "NAWAB ZADA HACKER 🦅🙌",
+  ownerNumber: "971528243405@s.whatsapp.net",
+  
+  // APNA WHATSAPP CHANNEL LINK AUR JID YAHAN LAGAO
+  channelLink: "https://whatsapp.com/channel/0029VbB47ttDDmFNztpnZf2m",
+  channelJid: "120363XXXXXXXXX@newsletter",
+  
+  version: "5.0.0",
+  prefix: ".",
+  
+  // Bot tracking
+  antilinkEnabled: {},
+  antilinkWarnings: {},
+  connectedUsers: []
+};
+
+module.exports = config;
